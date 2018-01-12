6a4a1778a7746a19c9b1caa70a3078405d481265
RachataLee <taza009@live.com>
2018-01-12 16:47:26 +0700

    refactor & เพิ่ม interval

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
+import React, { Component } from 'react';
+import Ticker from './Ticker'
+import axios from 'axios'
 import _ from 'lodash'
-  
-  class App extends Component {
-   SYMBOLS = ['OMG', 'BTC', 'XRP']
- 
-    constructor(props){
-      super(props)
-      this.state = {}
-  class App extends Component {
-      let self = this
-      axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
-      .then(function (response) {
-       let btc = _.find(response.data, ['symbol', 'BTC'])
-       let omg = _.find(response.data, ['symbol', 'OMG'])
-       self.setState({
-         BTC: btc.price_thb,
-         OMG: omg.price_thb
-       let price_state = {}
-       _.each(self.SYMBOLS, symbol => {
-         price_state[symbol] = _.find(response.data, ['symbol', symbol]).price_thb
-        })
-       self.setState(price_state)
-      })
-      .catch(function (error) {
-        console.log(error);
-   class App extends Component {
-    render() {
-      return (
-        <div>
-         <Ticker symbol="BTC" price={this.state.BTC}/>
-         <Ticker symbol="OMG" price={this.state.OMG}/>
-         {
+
+class App extends Component {
+  SYMBOLS = ['OMG', 'BTC', 'XRP']
+
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  //refactor
+  extractPrice(data){
+    let price_state = {}
+    _.each(this.SYMBOLS, symbol => {
+      price_state[symbol] = _.find(data, ['symbol', symbol]).price_thb
+    })
+    this.setState(price_state)
+  }
+
+  loadPrice(){
+    let self = this
+    axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
+    .then(function (response) {
+      self.extractPrice(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  componentDidMount(){
+    setInterval(this.loadPrice.bind(this), 3000)
+  }
+
+  render() {
+    return (
+      <div>
+        {
           _.map(this.SYMBOLS, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
-         }
-        </div>
-      );
-    }
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
